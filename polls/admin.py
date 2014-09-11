4b3e9cf48d8a120a4cb4fee7ff8934698691acf5
diek <diek@bellaliant.net>
2014-09-11 00:09:00 -0300

    fixed typo

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from polls.models import Choice, Question
 
 
-class ChoiceInLine(admin.StackedInLine):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -10,7 +10,8 @@ class ChoiceInLine(admin.StackedInLine):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date']})
+        ('Date Information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
