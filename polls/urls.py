91e8657efd36465b45d08d24eabb431de9b8fbb1
diek <diek@bellaliant.net>
2014-09-12 20:11:28 -0300

    Updated views

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,13 @@ from polls import views
 
 #The next step is to point the root URLconf at the polls.urls module.
 # In mysite/urls.py insert an include(),
-urlpatterns = patterns('', url(r'^$', views.index, name='index'), )
+urlpatterns = patterns('',
+    # ex
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    #ex: /polls/5/vote/
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+)
